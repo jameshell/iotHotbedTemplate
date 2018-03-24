d3bda10dccde38c0062f5e7495e0094fd234c494
James <jaimea111@gmail.com>
2018-03-23 20:14:54 -0500

    Initial code

---
+++ navigationPageExample/navigationPageExample/Views/Page1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace navigationPageExample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var page = new Views.Page2();
+            NavigationPage.SetHasNavigationBar(page,false);
+            Navigation.PushAsync(page);
+            
+        }
+    }
+}
